afc23dd572c1742d8283ab843ec4b516c5486bcb
tobias <Tobias.hassberg@web.de>
2022-03-14 16:45:20 +0100

    Dataset preperation and inclusion into sampling routine

--- file_sampler.py
+++ file_sampler.py
@@ -6,9 +6,10 @@ import numpy as np
 
 
 def sample_files(source_name: str, target_folder: str = "samples", samples=1, coloumns_to_drop=[],
-                 inlier_classes=[], max_data: int = 950, inlier_fraction=0.95, whitespace_delim: bool = False):
+                 inlier_classes=[], max_data: int = 1000, inlier_fraction=0.95, whitespace_delim: bool = False):
     file = pd \
-        .read_csv(os.path.join(os.path.dirname(__file__), "datasets", source_name), delim_whitespace=whitespace_delim) \
+        .read_csv(os.path.join(os.path.dirname(__file__), "datasets", source_name), delim_whitespace=whitespace_delim,
+                  index_col=False) \
         .drop(columns=coloumns_to_drop) \
 
     file.insert(len(file.columns)-1, 'class', file.pop('class'))
@@ -38,9 +39,9 @@ def sample_files(source_name: str, target_folder: str = "samples", samples=1, co
 
         samples = obj[samples_indices]
 
-        filename = source_name.split(".")[0] + "_sample_" + str(i) + "_data-points_" + str(
-            len(samples)) + "_inlier-classes_" + str(inlier_classes) + "_inlier-fraction_" + str(
-            inlier_fraction) + ".csv"
+        classes = ','.join(str(e) for e in inlier_classes)
+        filename = source_name.split(".")[0] + "-" + classes + "_sample_" + str(i) + "_data-points_" + str(
+            len(samples)) + "_inlier-fraction_" + str(inlier_fraction) + ".csv"
 
         pd.DataFrame(samples).to_csv(os.path.join(target_folder, filename), index=False, header=False)
 
@@ -48,8 +49,26 @@ def sample_files(source_name: str, target_folder: str = "samples", samples=1, co
 # list meanings: str-file name, bool - whitespace_delim, list - drop columns, list - inlier classes
 datasets = [
     ["glass.data", False, ["index"], [1]],
+    ["glass.data", False, ["index"], [2]],
     ["ecoli.data", True, ["sample_machine"], ["cp"]],
+    ["ecoli.data", True, ["sample_machine"], ["im"]],
+    ["page-blocks.data", True, [], [1]],
+    ["page-blocks.data", True, [], [2]],
     ["lymphography.data", False, [], [2]],
+    ["lymphography.data", False, [], [3]],
+    ["ionosphere.data", False, [], ["g"]],
+    ["ionosphere.data", False, [], ["b"]],
+    ["yeast.data", True, ["sample_machine"], ["CYT"]],
+    ["yeast.data", True, ["sample_machine"], ["NUC"]],
+    ["yeast.data", True, ["sample_machine"], ["ME3"]],
+    ["yeast.data", True, ["sample_machine"], ["MIT"]],
+    ["wdbc.data", False, ["ID"], ["B"]],
+    ["wdbc.data", False, ["ID"], ["M"]],
+    ["wpbc.data", False, ["ID"], ["N"]],
+    ["wpbc.data", False, ["ID"], ["R"]],
+    ["waveform.data", False, [], [0]],
+    ["waveform.data", False, [], [1]],
+    ["waveform.data", False, [], [2]],
 ]
 
 for dataset in datasets:
