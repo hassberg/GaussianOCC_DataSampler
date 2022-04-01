c300c6fc816fb8249279af04dc10c743d0c41f94
tobias <Tobias.hassberg@web.de>
2022-04-01 11:46:29 +0200

    groundtruth sampling for continuous data

--- file_sampler.py
+++ file_sampler.py
@@ -34,7 +34,7 @@ def create_sample(inlier_indices, outlier_indices, data_obj, output_path, sample
     pd.DataFrame(samples_test).to_csv(os.path.join(final_folder, "test.csv"), index=False, header=False)
 
 
-def sample_files(source_name: str, target_folder: str, samples=5, coloumns_to_drop=[],
+def sample_files(source_name: str, target_folder: str, sampling_strategy: str, samples=5, coloumns_to_drop=[],
                  inlier_classes=[], whitespace_delim: bool = False):
     file = pd \
         .read_csv(os.path.join(os.path.dirname(__file__), "datasets", source_name), delim_whitespace=whitespace_delim, index_col=False) \
@@ -63,6 +63,12 @@ def sample_files(source_name: str, target_folder: str, samples=5, coloumns_to_dr
     if not os.path.exists(path):
         os.makedirs(path)
 
+    if sampling_strategy == "continuous":
+        gt_path = os.path.join(path, "ground_truth")
+        if not os.path.exists(gt_path):
+            os.makedirs(gt_path)
+        pd.DataFrame(obj).to_csv(os.path.join(gt_path, "ground_truth.csv"), index=False, header=False)
+
     ## sample train
     create_sample(inlier_indices=inlier, outlier_indices=outlier, sample_run="test", creating_test=True, data_obj=obj, output_path=path)
     for i in range(samples):
@@ -70,36 +76,37 @@ def sample_files(source_name: str, target_folder: str, samples=5, coloumns_to_dr
         create_sample(inlier_indices=inlier, outlier_indices=outlier, sample_run="sample_" + str(i), data_obj=obj, output_path=path)
 
 
-# list meanings: str-file name, bool - whitespace_delim, list - drop columns, list - inlier classes
+# list meanings: str-file name, bool - whitespace_delim, list - drop columns, list - inlier classes - sampling_type
 datasets = [
-    # ["breast-cancer-wisconsin.data", False, ["code"], [2]],
-    # ["breast-cancer-wisconsin.data", False, ["code"], [4]],
-    ["ecoli.data", True, ["sample_machine"], ["cp"]],
-    # ["ecoli.data", True, ["sample_machine"], ["im"]],
-    # ["glass.data", False, ["index"], [1, 2, 3, 4]],
-    # ["glass.data", False, ["index"], [2]],
-    # ["ionosphere.data", False, [], ["g"]],
-    # ["ionosphere.data", False, [], ["b"]],
-    # ["lymphography.data", False, [], [2]],
-    # ["lymphography.data", False, [], [3]],
-    # ["page-blocks.data", True, [], [1]],
-    # ["page-blocks.data", True, [], [2]],
-    # ["svmguide1.csv", False, [], [1]],
-    # ["svmguide1.csv", False, [], [1]],
-    # ["waveform.data", False, [], [0]],
-    # ["waveform.data", False, [], [1]],
-    # ["waveform.data", False, [], [2]],
-    # ["wdbc.data", False, ["ID"], ["B"]],
-    # ["wdbc.data", False, ["ID"], ["M"]],
-    # ["wpbc.data", False, ["ID"], ["N"]],
-    # ["wpbc.data", False, ["ID"], ["R"]],
-    # ["yeast.data", True, ["sample_machine"], ["CYT"]],
-    # ["yeast.data", True, ["sample_machine"], ["NUC"]],
-    # ["yeast.data", True, ["sample_machine"], ["ME3"]],
-    # ["yeast.data", True, ["sample_machine"], ["MIT"]],
+    # ["breast-cancer-wisconsin.data", False, ["code"], [2], "discrete", ],
+    # ["breast-cancer-wisconsin.data", False, ["code"], [4], "discrete", ],
+    ["ecoli.data", True, ["sample_machine"], ["cp"], "discrete", ],
+    # ["ecoli.data", True, ["sample_machine"], ["im"], "discrete", ],
+    # ["glass.data", False, ["index"], [1, 2, 3, 4], "discrete", ],
+    # ["glass.data", False, ["index"], [2], "discrete", ],
+    # ["ionosphere.data", False, [], ["g"], "discrete", ],
+    # ["ionosphere.data", False, [], ["b"], "discrete", ],
+    # ["lymphography.data", False, [], [2], "discrete", ],
+    # ["lymphography.data", False, [], [3], "discrete", ],
+    # ["page-blocks.data", True, [], [1], "discrete", ],
+    # ["page-blocks.data", True, [], [2], "discrete", ],
+    # ["svmguide1.csv", False, [], [1], "discrete", ],
+    ["svmguide1.csv", False, [], [1], "continuous", ],
+    # ["svmguide1.csv", False, [], [1], "discrete", ],
+    # ["waveform.data", False, [], [0], "discrete", ],
+    # ["waveform.data", False, [], [1], "discrete", ],
+    # ["waveform.data", False, [], [2], "discrete", ],
+    # ["wdbc.data", False, ["ID"], ["B"], "discrete", ],
+    # ["wdbc.data", False, ["ID"], ["M"], "discrete", ],
+    # ["wpbc.data", False, ["ID"], ["N"], "discrete", ],
+    # ["wpbc.data", False, ["ID"], ["R"], "discrete", ],
+    # ["yeast.data", True, ["sample_machine"], ["CYT"], "discrete", ],
+    # ["yeast.data", True, ["sample_machine"], ["NUC"], "discrete", ],
+    # ["yeast.data", True, ["sample_machine"], ["ME3"], "discrete", ],
+    # ["yeast.data", True, ["sample_machine"], ["MIT"], "discrete", ],
 ]
 
 for dataset in datasets:
-    sample_files(dataset[0], target_folder="samples/discrete",
+    sample_files(dataset[0], target_folder="samples/" + dataset[4], sampling_strategy=dataset[4],
                  whitespace_delim=dataset[1], samples=10,
                  coloumns_to_drop=dataset[2], inlier_classes=dataset[3])
